5a45e130cc427571d5ad3296646943aa7d2e8518
cathleen turner <turner.cathleen@gmail.com>
2017-11-21 01:01:17 +0000

    labels on buttons

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,35 @@ var hotel = {
     name: "CareerDevs Innt"
 }
 
+function createRadioButtons(){
+    var content;
+    var divItems = document.getElementById("radialSection");
+    for (var i = 0; i < hotel.rooms.length; i++ ){
+        var room = hotel.rooms[i];
+        var name = room.name;
+        var radioBtn = document.createElement("INPUT");
+        // use the id as value so you can use
+        // programmatically
+        radioBtn.setAttribute("value", i);
+        radioBtn.setAttribute("id", `radio-${i}`);
+        radioBtn.setAttribute("type", "radio");
+        // use name so that you can only select one radio at a time
+        radioBtn.setAttribute("name","room");
+        var radioLbl = document.createElement("LABEL");
+        //radioLbl.setAttribute("id", `label-${i}`);
+        radioLbl.innerHTML = name;
+        
+        //this doesnt work bc radio btns dont have inner html
+        //radioBtn.appendChild(radioLbl);
+        
+        
+        divItems.appendChild(radioBtn);
+        divItems.appendChild(radioLbl)
+        
+        
+    }
+    
+}
+
+createRadioButtons()
+
